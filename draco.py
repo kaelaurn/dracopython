3225cd5b228ebac82a3d483de1a44de569f307b3
Kaelaurn <kaue.rago1@gmail.com>
2021-12-01 22:50:18 -0300

    test

--- draco.py
+++ draco.py
@@ -1,13 +1,24 @@
 # draco
 try:
-    draco = float(input('Entre valor Draco: '))
-    dracon = float(input('Quantos dracos possui: '))
-    dolar = float(input('Valor Dolar: '))
-    dracov = dracon * (dolar * draco)
-    porcentagem = dracov * 1.7 / 100
-    resultado = dracov - porcentagem
-    print("Sem taxa:",dracov)
-    print("Com taxa:",resultado)
+    draco = (input('Deseja calcular o Draco ou Darksteel?')).lower()
+    if draco== "draco":
+        draco = float(input('Entre valor Draco: '))
+        dracon = float(input('Quantos dracos possui: '))
+        dolar = float(input('Valor Dolar: '))
+        dracov = dracon * (dolar * draco)
+        porcentagem = dracov * 1.7 / 100
+        resultado = dracov - porcentagem
+        print("Sem taxa:",dracov)
+        print("Com taxa:",resultado)
+    elif draco== "darksteel":
+     print('Calculando valor do Draco para Darksteel')   
+     draco = float(input('Entre quantos Dracos: ')) 
+     darksteel = 104.921 * draco 
+     perdeu= (draco * 105.921) - darksteel
+     print("Darksteel:",darksteel)
+     print("Perdeu:",perdeu) 
+    else:
+        print("Digite draco ou darksteel")
 except ValueError:
-    print("Valor inválido!")
+    print('Valor Inválido')
 input('Pressione ENTER para sair...')
